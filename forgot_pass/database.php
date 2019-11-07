731c92b53e3aa2c5cc2503c9db9c687be4691f5f
soumyababu <39803577+soumyababu@users.noreply.github.com>
2019-11-07 10:31:39 +0530

    Add files via upload

---
+++ forgot_pass/database.php
@@ -0,0 +1,11 @@
+<?php
+$servername='localhost';
+$username='root';
+$password='';
+$dbname = "crud";
+$conn=mysqli_connect($servername,$username,$password,"$dbname");
+if(!$conn){
+ die('Could not Connect My Sql:' .mysql_error());
+}
+
+?>
